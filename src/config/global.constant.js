fb24522013607fd8dfb115524c43c1c4e916276c
Badluxus <mamadousadaw@gmail.com>
2024-12-30 21:07:47 +0000

    Update settings

--- src/config/global.constant.js
+++ src/config/global.constant.js
@@ -1,3 +1,4 @@
-export const API_URL = `http://localhost:3001/users`;
-export const API_URL_LOGIN = `http://localhost:3001/login`;
-// export const API_URL = `https://projetnosqlback.onrender.com/users`;
+// export const API_URL = `http://localhost:3001/users`;
+// export const API_URL_LOGIN = `http://localhost:3001/login`;
+export const API_URL = `https://projetnosqlback.onrender.com/users`;
+export const API_URL_LOGIN = `https://projetnosqlback.onrender.com/login`;
